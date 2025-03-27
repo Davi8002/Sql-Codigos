c0fddb3814b2ea463a94682102d78ae62b4fda21
Davi <164496370+Davi8002@users.noreply.github.com>
2025-03-27 11:26:35 -0300

    Update codigo1.sql

--- codigo1.sql
+++ codigo1.sql
@@ -8,6 +8,7 @@ CREATE TABLE Paciente(
   Cpf           varchar(70)
 );
 
+INSERT INTO Paciente VALUES (01,"Nome", "01/10/2008", "email@gmail.com",5581989639664, "12312312312");
 
 CREATE TABLE Medico(
   IdM    		INT 	PRIMARY KEY,
@@ -19,6 +20,8 @@ CREATE TABLE Medico(
   Salario       int,
   Horario       varchar(50),
   Cpf           varchar(30));
+
+INSERT INTO Medico VALUES (01,"Nome", "01/10/2008", "email@gmail.com",5581989639664, "formacao", "10", "12:00 as 20:00)", "12312312312");
 
 
 CREATE TABLE Enfermeiro(
@@ -33,7 +36,7 @@ CREATE TABLE Enfermeiro(
   Cpf           varchar(30)
 );
 
-INSERT INTO Enfermeiro VALUES (01,"Nome", "01/10/2008", "email@gmail.com",5581989639664, "formacao", "10", "12:00 as 20:00)", "12312312312")
+INSERT INTO Enfermeiro VALUES (01,"Nome", "01/10/2008", "email@gmail.com",5581989639664, "formacao", "10", "12:00 as 20:00)", "12312312312");
 
 CREATE TABLE Receita(
   IdR   		INT 	PRIMARY KEY,
@@ -41,3 +44,5 @@ CREATE TABLE Receita(
   Data_Emissao  varchar(20),
   Medicamentos        varchar(70)
 );
+
+INSERT INTO Receita VALUES (01, 200, "27/03/2025", "Heh");
