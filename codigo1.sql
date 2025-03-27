a34da781cc9a33998aee451bcc3f87e784bd5c7b
Davi <164496370+Davi8002@users.noreply.github.com>
2025-03-27 07:51:12 -0300

    Update codigo1.sql

--- codigo1.sql
+++ codigo1.sql
@@ -1,1 +1,12 @@
 
+drop table nomedatabela
+
+
+
+CREATE TABLE Paciente(
+  Cpf			INT 	PRIMARY KEY,
+  Nome          varchar(150),
+  Data_Nasci    Date,
+  Email         varchar(70),
+  Telefone      INT
+)
