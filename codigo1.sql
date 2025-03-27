12f0236d76736bbf05a152576ea1c10e267868c7
Davi <164496370+Davi8002@users.noreply.github.com>
2025-03-27 10:52:33 -0300

    Update codigo1.sql

--- codigo1.sql
+++ codigo1.sql
@@ -1,12 +1,40 @@
 
-drop table nomedatabela
-
-
-
 CREATE TABLE Paciente(
-  Cpf			INT 	PRIMARY KEY,
+  IdP			INT 	PRIMARY KEY,
   Nome          varchar(150),
-  Data_Nasci    Date,
+  Data_Nasci    varchar(20),
   Email         varchar(70),
-  Telefone      INT
-)
+  Telefone      INT,
+  Cpf           varchar(70)
+);
+
+
+CREATE TABLE Medico(
+  IdM    		INT 	PRIMARY KEY,
+  Nome          varchar(150),
+  Data_Nasci    varchar(20),
+  Email         varchar(70),
+  Telefone      INT,
+  Formacao      varchar(70),
+  Salario       int,
+  Horario       varchar(50)
+);
+
+
+CREATE TABLE Enfermeiro(
+  IdE   		INT 	PRIMARY KEY,
+  Nome          varchar(150),
+  Data_Nasci    varchar(20),
+  Email         varchar(70),
+  Telefone      INT,
+  Formacao      varchar(70),
+  Salario       int,
+  Horario       varchar(50)
+);
+
+CREATE TABLE Receita(
+  IdR   		INT 	PRIMARY KEY,
+  Precos        INT,
+  Data_Emissao  varchar(20),
+  Medicamentos        varchar(70)
+);
