d1872e4420246bae570633b2ad489a570d7f9b3f
Davi <164496370+Davi8002@users.noreply.github.com>
2025-04-01 07:44:45 -0300

    Update codigo2.sql

--- codigo2.sql
+++ codigo2.sql
@@ -1,1 +1,22 @@
 
+CREATE TABLE Personagem(
+  IdP			INT 	PRIMARY KEY,
+  Nome          varchar(70),
+  Classe        varchar(50),
+  Id_Status     INT,
+  Rank          varchar(50),
+  FOREIGN KEY (Id_Status) REFERENCES Status(Id_Status)
+);
+
+CREATE TABLE Status(
+  Id_Status			INT 	PRIMARY KEY,
+  Hp   VARCHAR(31),
+  Mp   VARCHAR(31),
+  Fadiga   VARCHAR(31),
+  Forca INT,
+  Agilidade INT,
+  Percepcao INT,
+  Vitalidade INT,
+  Intelecto INT,
+  Pontos_Disponiveis INT
+);
