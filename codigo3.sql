0b821a4c2b95a9a25dc8127bd41c6668e7ceed88
Davi <164496370+Davi8002@users.noreply.github.com>
2025-04-10 19:01:26 -0300

    Update codigo3.sql

--- codigo3.sql
+++ codigo3.sql
@@ -91,16 +91,16 @@ INSERT INTO Turma (Id_Turma, Ano_Letivo, Quantidade_Alunos, Codigo, Turno, Sala)
 VALUES (1, '2025', 30, '123', 'Manhã', '101');
 
 INSERT INTO Responsavel (Id_Responsavel, Nome, Email, Senha, Data_Nascimento, Telefone, Cpf)
-VALUES (1, 'NomeDeResponsavel', 'emaildoresponsavel@email.com', 'responsavelhehe123', '1980-05-20', 11987654321, 12345678900);
+VALUES (1, 'NomeDeResponsavel', 'emaildoresponsavel@gmail.com', 'responsavelhehe123', '1980-05-20', 11987654321, 12345678900);
 
 INSERT INTO Professor (Id_Professor, Nome, Email, Senha, Data_Nascimento, Telefone, Cpf)
-VALUES (1, 'NomedeProfessor', 'professor@email.com', 'sololeveling123', '1990-10-12', 11912345678, 98765432100);
+VALUES (1, 'NomedeProfessor', 'professor@gmail.com', 'sololeveling123', '1990-10-12', 11912345678, 98765432100);
 
 INSERT INTO Disciplina (Id_Disciplina, Id_Professor, Nome)
 VALUES (1, 1, 'Matemática');
 
 INSERT INTO Aluno (Id_Aluno, Email, Nome, Senha, Data_Nascimento, Id_Turma, Id_Responsavel)
-VALUES (1, 'aluno1@email.com', 'Davi Ribeiro', 'senha123', '2008-01-10', 1, 1);
+VALUES (1, 'aluno1@gmail.com', 'Davi Ribeiro', 'senha123', '2008-01-10', 1, 1);
 
 INSERT INTO Atividade (Id_Atividade, Id_Disciplina, Nome, Descricao, Data_Maxima, Nota_Max)
 VALUES (1, 1, 'Projeto de banco de dados', 'Fazer um projeto de banco de dados', '2025-04-10', 10);
